08d15e4a7eb9ef1611d2c0a1824d0847bbcebfcd
JS Kim <splineage@naver.com>
2022-05-10 18:23:36 +0900

    const and let

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // variables
-const a = 5;
-const b = 5;
-const name = "test"
+// var: duplicate declare, function-level scope(no block scope), must not be used.
+// Declare all local variables with either const or let.
+// Use const by default, unless a variable needs to be reassigned.
+let a = 5;
+let b = 5;
+let name = "test"
+a=4;
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
+name = "test2"
 console.log("hello " + name);
