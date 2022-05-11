1f7fcf8a5d264ba33326cc2fb9a56018f31c33a6
JS Kim <splineage@naver.com>
2022-05-11 14:11:35 +0900

    event

--- main.js
+++ main.js
@@ -36,5 +36,53 @@ console.log(tagNameTwo);
 const tagNameThree = document.querySelector("#title"); 
 // same
 // const tagNameThree = document.getElementById("hello"); 
-
 console.log(tagNameThree);
+
+const h2 = document.querySelector("div.hello:first-child h2");
+console.log(h2);
+console.dir(h2);
+// event
+// h2 html element mdn
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement
+// https://developer.mozilla.org/en-US/docs/Web/API/Element
+
+
+h2.style.color = "blue" // change style, but CSS 에서 하는게 좋음.
+
+function handleTitleClick(){
+    h2.style.color = "red";
+    console.log("title was clicked");
+}
+function handleMouseEnter(){
+    console.log("mouseenter")
+    h2.innerText = "Mouse is here!"
+}
+function handleMouseLeave(){
+    console.log("mouseleave");
+    h2.innerText = "Mouse is gone!"
+}
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+function handleWindowCopy(){
+    alert("copier!");
+}
+function handleWindowOffline(){
+    alert("no Wifi")
+}
+function handleWindowOnline(){
+    alert("good")
+}
+
+// h2.addEventListener("click", handleTitleClick) 
+h2.onclick = handleTitleClick
+// h2.addEventListener("mouseenter", handleMouseEnter);
+h2.onmouseenter = handleMouseEnter
+h2.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+//clipboard events
+window.addEventListener("copy", handleWindowCopy);
+// connection events
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
