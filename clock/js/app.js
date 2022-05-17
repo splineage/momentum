e03141ae0cd1acd360217abb825cdc6676c51dcf
JS Kim <splineage@naver.com>
2022-05-17 14:38:26 +0900

    add something

--- clock/js/app.js
+++ clock/js/app.js
@@ -1,4 +1,4 @@
-const clock = document.querySelector("h2#clock")
+const clock = document.querySelector('h2#clock')
 
 // interval (ms)
 // 5초마다 input function 실행.
@@ -16,7 +16,9 @@ function sayHello(){
 
 function getClock(){
     const date = new Date();
-    const hour = date.getHours().toString().padStart(2,'0');
+    // String & toString : 형변환할때는 차이가 없음.
+    // toString 의 경우 진수 변환 가능.
+    const hour = date.getHours().toString().padStart(2,'0'); // ', " 차이없음. ` 내부 변수선언 가능.
     const minute = date.getMinutes().toString().padStart(2,'0');
     const second = date.getSeconds().toString().padStart(2,'0');
     const time = `${hour}:${minute}:${second}`;
