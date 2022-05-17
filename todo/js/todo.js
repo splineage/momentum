3ab22b4aed53da2dfc39422355fcdb64ecf804ed
JS Kim <splineage@naver.com>
2022-05-17 16:26:49 +0900

    add delete todolist

--- todo/js/todo.js
+++ todo/js/todo.js
@@ -2,15 +2,25 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+function deleteToDo(event){
+    // which button clicked?
+    console.dir(event.target.parentElement);
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo){
     // console.log(`paint ${newTodo}`);
     const li = document.createElement('li');
     const insideSpan = document.createElement('span');
+    const button = document.createElement('button');
+    button.innerText = "❌";
+    button.addEventListener('click', deleteToDo)
     li.appendChild(insideSpan);
+    li.appendChild(button);
+    toDoList.appendChild(li);
     insideSpan.innerText = newTodo;
     console.log(insideSpan);
-    toDoList.appendChild(li);
-
 }
 function handleTodoSubmit(event){
     event.preventDefault();
